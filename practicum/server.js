0f1cfa7977cf0fd6de12a393c6b4897cdd88c5ae
Tomvh123 <thaaster@avans.nl>
2017-05-30 23:58:44 +0200

    test

--- practicum/server.js
+++ practicum/server.js
@@ -1,9 +1,31 @@
 /**
  * Created by tom on 30-5-2017.
  */
-
+var port = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
+
+app.get('/json', function(request, response) {
+    response.json({
+        'some_name': 'Value',
+        'an_array_of_objects': [
+            {
+                'another_name': 'Another value',
+                'a_further_name': "A further value"
+            },
+            {'yet_another_name': 'Yet another value'}
+        ],
+        'some_boolean': true,
+        'some_integer': 42,
+        'array_of_ints': [
+            2, 3, 5, 7, 11, 13
+        ],
+        'array_of_strings': [
+            "twee", "drie", "vijf", "zeven"
+        ]
+    })
+})
+
 app.get('/', function(request, response) {
     response.send('Hello Avans!');
 });
@@ -21,8 +43,8 @@ app.all('*', function (request, response) {
     response.send('404 - Not found');
 
 })
-app.listen(3000, function() {
-    console.log('Server app is listening on port 3000');
+app.listen(port, function() {
+    console.log('Server app is listening on port ' + port);
 });
 
 
