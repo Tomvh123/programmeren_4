ea7eaec7525fc9620329a935c25c0031b0a03542
Tomvh123 <thaaster@avans.nl>
2017-05-30 23:30:45 +0200

    Initial commit

---
+++ practicum/server.js
@@ -0,0 +1,28 @@
+/**
+ * Created by tom on 30-5-2017.
+ */
+
+var express = require('express');
+var app = express();
+app.get('/', function(request, response) {
+    response.send('Hello Avans!');
+});
+app.get('/about', function(request, response) {
+    response.send('Written by Tom van Haaster');
+});
+app.post('/', function(request, response) {
+    response.send('Hello Avans, POST request received!');
+});
+app.put('/', function(request, response) {
+    response.send('Hello Avans, PUT request received!');
+});
+
+app.all('*', function (request, response) {
+    response.send('404 - Not found');
+
+})
+app.listen(3000, function() {
+    console.log('Server app is listening on port 3000');
+});
+
+
